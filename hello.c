526c63ebed34730bd5e4fa25bf44c5454c73c337
SoeunLee <soeun2327@gmail.com>
2017-09-18 19:41:32 +0900

    my strcat 구

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 #include "hello.h"
 
 int main() {
-    printf("hello world\n");
+	printf("hello world\n");
 }
 
 char *myStrcpy(char *dest, const char *src) {
@@ -13,7 +13,11 @@ size_t myStrlen(const char *string) {
 }
 
 char *myStrcat(char *dest, const char *src) {
-    return NULL;
+	char *tmp = dest;
+	while(*tmp) tmp++;
+	while(*src) *tmp++ = *src++;
+	printf("%s", dest);
+	return dest;
 }
 
 int myStrcmp(const char *string1, const char *string2) {
