70521a0a550a405560df88e6d622411e3cb1982c
DESKTOP-C9S3J6P\sangeun <biglove97@daum.net>
2017-09-18 19:23:46 +0900

    strcmp

--- hello.c
+++ hello.c
@@ -17,5 +17,10 @@ char *myStrcat(char *dest, const char *src) {
 }
 
 int myStrcmp(const char *string1, const char *string2) {
+	for (int i = 0; string1[i]!=0; i++) {
+		if (string1[i] - string2[i] != 0) {
+			return string1[i] - string2[i];
+		}
+	}
     return 0;
 }
